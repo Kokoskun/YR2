e0049246fbb3f96971bfd01281b1a1229226bc08
Patipan Nakarin <kokoskun@gmail.com>
2018-06-17 12:23:29 +0700

    SiriFullStack

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,22 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <iframe width="933" height="700" src="https://msit.powerbi.com/view?r=eyJrIjoiMjdiODU4ZjAtMDFiYy00MmQ2LWJlNGYtMmZkMDYzNmY1MDU4IiwidCI6IjcyZjk4OGJmLTg2ZjEtNDFhZi05MWFiLTJkN2NkMDExZGI0NyIsImMiOjV9" frameborder="0" allowFullScreen="true"></iframe>
+            <dir class="col-md-12">
+                <h4><b>การนัดรับวัคซีน: </b></h4>
+                <iframe style="width:100%;" height="700" src="https://msit.powerbi.com/view?r=eyJrIjoiMjdiODU4ZjAtMDFiYy00MmQ2LWJlNGYtMmZkMDYzNmY1MDU4IiwidCI6IjcyZjk4OGJmLTg2ZjEtNDFhZi05MWFiLTJkN2NkMDExZGI0NyIsImMiOjV9" frameborder="0" allowFullScreen="true"></iframe>
+            </dir>
+            <dir class="col-md-12">
+                <h4><b>จำนวนคนงานแต่ละจังหวัด: </b></h4>
+                <iframe style="width:100%;" height="700" src="https://msit.powerbi.com/view?r=eyJrIjoiNTc4MDk5ODQtMTljMy00NzUwLWJkODYtODUwNTEwMjI0Y2YxIiwidCI6IjcyZjk4OGJmLTg2ZjEtNDFhZi05MWFiLTJkN2NkMDExZGI0NyIsImMiOjV9" frameborder="0" allowFullScreen="true"></iframe>
+            </dir>    
+            <dir class="col-md-12">
+                <h4><b>โครงการเข้าร่วมกับ Good Space: </b></h4>
+                <iframe style="width:100%;" height="700" src="https://msit.powerbi.com/view?r=eyJrIjoiMDQyYjY1MjMtYjBjOS00MTVmLWI2MmMtOTBmNzc1NmRmMmE3IiwidCI6IjcyZjk4OGJmLTg2ZjEtNDFhZi05MWFiLTJkN2NkMDExZGI0NyIsImMiOjV9" frameborder="0" allowFullScreen="true"></iframe>
+            </dir>  
+            <dir class="col-md-12">
+                <h4><b>สัดส่วนพนักงานตามสัญชาติและพนักงานต่อผู้ติดตาม: </b></h4>
+                <iframe style="width:100%;" height="700" src="https://msit.powerbi.com/view?r=eyJrIjoiNGEyNWFlMzEtZGYzMC00ZWIwLWFiYzEtMjhiY2NkMzk0ZDNkIiwidCI6IjcyZjk4OGJmLTg2ZjEtNDFhZi05MWFiLTJkN2NkMDExZGI0NyIsImMiOjV9" frameborder="0" allowFullScreen="true"></iframe>
+            </dir>  
         </div>
     </div>
 </div>
